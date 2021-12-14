0768a747530134b0a40997f061a969564a1ffe11
mgeaghan <michael.p.geaghan@gmail.com>
2021-12-15 09:59:59 +1100

    add putative variant filter

--- whitelist/whitelist_filter_rscript.R
+++ whitelist/whitelist_filter_rscript.R
@@ -935,6 +935,51 @@ pwl <- wl_ns_df[, 3] | wl_fs_df[, 3] | wl_sg_df[, 3] | wl_sp_df[, 3]
 pmr <- (!pwl) & (wl_ns_df[, 4] | wl_fs_df[, 4] | wl_sg_df[, 4] | wl_sp_df[, 4])
 overall_wl <- data.frame(Whitelist = wl, Manual_Review = mr, Putative_Whitelist = pwl, Putative_Manual_Review = pmr)
 
+# Apply stricter hard filter to putative CHIP variants
+vars_g_chip_func_filtered$PUTATIVE <- overall_wl$Putative_Whitelist | overall_wl$Putative_Manual_Review
+vars_g_chip_func_filtered$PUTATIVE_FILTER <- apply(vars_g_chip_func_filtered[, c("PUTATIVE", "AD", "F1R2", "F2R1", "VAF")], 1, function(x) {
+  p <- as.logical(x[[1]])
+  ad <- as.integer(strsplit(x[[2]], ",")[[1]])
+  f1r2 <- as.integer(strsplit(x[[3]], ",")[[1]])
+  f2r1 <- as.integer(strsplit(x[[4]], ",")[[1]])
+  vaf <- as.numeric(x[[5]])
+  if (!p) {
+    return("PASS")
+  }
+  # Allelic depth
+  var_n_ad_gte_5 <- ad >= 5
+  # VAF
+  var_n_vaf_lte_20pc <- vaf <= 0.2
+  # Forward and reverse strand support
+  var_n_for_rev_gte_2 <- all(f1r2 >= 2) && all(f2r1 >= 2)
+  filter_pass <- (
+    all(var_n_ad_gte_5[2:length(var_n_ad_gte_5)]) &  # first allele = ref
+      all(var_n_vaf_lte_20pc) &
+      var_n_for_rev_gte_2
+  )
+  filter_manual_review <- (
+    any(var_n_ad_gte_5[2:length(var_n_ad_gte_5)]) &  # first allele = ref; if any, but not all pass, go to manual review
+      any(var_n_vaf_lte_20pc) &  # some but not all are passing
+      var_n_for_rev_gte_2
+  )
+  PUTATIVE_FILTER = "FAIL"
+  if(filter_pass) {
+    PUTATIVE_FILTER = "PASS"
+  } else if(filter_manual_review) {
+    PUTATIVE_FILTER = "MANUAL_REVIEW"
+  }
+  return(PUTATIVE_FILTER)
+})
+vars_g_chip_func_filtered$COMBINED_FILTER <- apply(vars[c("COMBINED_FILTER", "PUTATIVE_FILTER")], 1, function(x) {
+  if (x[[1]] == "PASS" && x[[2]] == "PASS") {
+    return("PASS")
+  } else if (x[[1]] == "FAIL" || x[[2]] == "FAIL") {
+    return("FAIL")
+  } else {
+    return("MANUAL_REVIEW")
+  }
+})
+
 overall_wl$Manual_Review <- (overall_wl$Manual_Review & vars_g_chip_func_filtered$COMBINED_FILTER != "FAIL") | (overall_wl$Whitelist & vars_g_chip_func_filtered$COMBINED_FILTER == "MANUAL_REVIEW")
 overall_wl$Whitelist <- overall_wl$Whitelist & vars_g_chip_func_filtered$COMBINED_FILTER == "PASS"
 overall_wl$Putative_Manual_Review <- (overall_wl$Putative_Manual_Review & vars_g_chip_func_filtered$COMBINED_FILTER != "FAIL") | (overall_wl$Putative_Whitelist & vars_g_chip_func_filtered$COMBINED_FILTER == "MANUAL_REVIEW")
