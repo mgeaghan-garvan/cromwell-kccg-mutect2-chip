457511e841bfcf33e00331b32d7a7bb9d116cb9f
mgeaghan <michael.p.geaghan@gmail.com>
2022-03-24 16:40:31 +1100

    first draft, wip

--- generate_dx_run_cmd.py
+++ generate_dx_run_cmd.py
@@ -6,11 +6,44 @@ import sys
 def check_args(args=None):
     parser = argparse.ArgumentParser(description="Generate the run command for DNAnexus.", formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("-j", "--json", help="Path to input JSON file.", required=True, type=str)
+    parser.add_argument("-b", "--batch", help="Path to a batch input TSV file for running multiple single-sample workflows in parallel. See the README and the template file at ./input/inputFiles.tsv for information on the required format.", default="", type=str)
     parser.add_argument("-d", "--destination", help="Path to output directory on the DNAnexus platform.", required=True, type=str)
     parser.add_argument("-w", "--workflow", help="Path to workflow on the DNAnexus platform.", required=True, type=str)
     parser.add_argument("-o", "--output", help="Path to bash file to output run command to. Default: _dx_run.sh.", default="_dx_run.sh", type=str)
     arguments = parser.parse_args(args)
     return arguments
+
+
+VALID_JSON_KEYS = [
+    "Mutect2CHIP",
+    "Mutect2CHIP_CHIP",
+    "Mutect2CHIP_Annovar",
+    "Mutect2CHIP_VEP"
+]
+
+JSON_KEY_MAP = {
+    "Mutect2CHIP": [
+        "tumor_reads",
+        "tumor_reads_index",
+        "normal_reads",
+        "normal_reads_index"
+    ],
+    "Mutect2CHIP_CHIP": [
+        "input_vcf",
+        "tumor_sample_name"
+    ],
+    "Mutect2CHIP_Annovar": [
+        "input_vcf"
+    ],
+    "Mutect2CHIP_VEP": [
+        "input_vcf",
+        "input_vcf_idx"
+    ]
+}
+
+
+def validate_batch_json(json_data):
+    pass
 
 
 def main(args):
--- run_dx.sh
+++ run_dx.sh
@@ -3,11 +3,12 @@
 # Usage
 helpmsg() {
     echo "Run the Mutect2 pipeline on DNAnexus."
-    echo -e "\nUsage: $0 [-w|--workflow PATH_TO_WORKFLOW] [-i|--input INPUT_JSON] [-o|--output OUTPUT_PATH]"
+    echo -e "\nUsage: $0 [-w|--workflow PATH_TO_WORKFLOW] [-i|--input INPUT_JSON] [-b|--batch BATCH_INPUT_FILE] [-o|--output OUTPUT_PATH]"
     echo -e "Display this help message: $0 -h\n"
     echo -e "\tPATH_TO_WORKFLOW:  Path to the workflow on the DNAnexus platform."
     echo -e "\tINPUT_JSON:        Input JSON file (use the templates in the input/ directory)."
     echo -e "\tOUTPUT_PATH:       Path to the output directory on the DNAnexus platform."
+    echo -e "\tBATCH_INPUT_FILE:  Path to a batch input TSV file for running multiple single-sample workflows in parallel. See the README and the template file at ./input/inputFiles.tsv for information on the required format."
     echo -e "\t[-d|--dryrun]:     Perform a dry-run by printing the DNAnexus run command to the terminal without running."
 }
 
@@ -22,6 +23,7 @@ fi
 DRYRUN="FALSE"
 PATH_TO_WORKFLOW=""
 INPUT_JSON=""
+BATCH_INPUT_FILE=""
 OUTPUT_PATH=""
 
 # Arguments
@@ -40,6 +42,11 @@ while [[ $# -gt 0 ]]; do
                         ;;
                 -i|--input)
                         INPUT_JSON="$2"
+                        shift
+                        shift
+                        ;;
+                -b|--batch)
+                        BATCH_INPUT_FILE="$2"
                         shift
                         shift
                         ;;
@@ -63,15 +70,31 @@ set -- "${POSITIONAL[@]}"
 
 if [ "${PATH_TO_WORKFLOW}" == "" ] || [ "${INPUT_JSON}" == "" ] || [ "${OUTPUT_PATH}" == "" ]; then echo "Missing parameters!"; exit; fi
 if [ ! -f "${INPUT_JSON}" ]; then echo "Invalid path to input JSON file!"; exit; fi
+if [ "${BATCH_INPUT_FILE}" != "" ] && [ ! -f "${BATCH_INPUT_FILE}" ]; then echo "Invalid path to batch TSV file!"; exit; fi
 
 # Create DNAnexus input JSON file
-python3 generate_dx_run_cmd.py -j ${INPUT_JSON} -d ${OUTPUT_PATH} -w ${PATH_TO_WORKFLOW} -o _dx_run.sh
-
-echo "DNAnexus run command:"
-cat _dx_run.sh
-chmod +x _dx_run.sh
-
-if [ "${DRYRUN}" == "FALSE" ]
+if [ "${BATCH_INPUT_FILE}" == "" ]
 then
-    ./_dx_run.sh
+    python3 generate_dx_run_cmd.py -j ${INPUT_JSON} -d ${OUTPUT_PATH} -w ${PATH_TO_WORKFLOW} -o _dx_run.sh
+    echo "DNAnexus run command:"
+    cat _dx_run.sh
+    chmod +x _dx_run.sh
+    if [ "${DRYRUN}" == "FALSE" ]
+    then
+        ./_dx_run.sh
+    fi
+else
+    CMD_FILE_LIST=$(python3 generate_dx_run_cmd.py -j ${INPUT_JSON} -b ${BATCH_INPUT_FILE} -d ${OUTPUT_PATH} -w ${PATH_TO_WORKFLOW} -o _dx_run.sh)
+    for FILE in ${CMD_FILE_LIST}
+    do
+        echo "===== ${FILE} ====="
+        echo "DNAnexus run command:"
+        cat ${FILE}
+        if [ "${DRYRUN}" == "FALSE" ]
+        then
+            bash ${FILE}
+        fi
+        echo "=========="
+    done
 fi
+
