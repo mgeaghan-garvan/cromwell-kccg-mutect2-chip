04f05591d1f78b8095892e11b6f8b5b3f081e71b
mgeaghan <michael.p.geaghan@gmail.com>
2024-01-16 13:15:04 +1100

    add script to move analysis outputs to relese, delete when no longer needed

--- scripts/cpg_cromwell/remove_analysis_outs.py
+++ scripts/cpg_cromwell/remove_analysis_outs.py
@@ -1,5 +1,5 @@
 """
-Move the CHIP pipeline outputs to the release bucket for export to the GML private buckets
+Remove the CHIP pipeline outputs from the release bucket once no longer needed
 """
 import hailtop.batch as hb
 from cpg_utils.config import get_config
@@ -15,7 +15,6 @@ RUN_ID = _config['workflow']['run_id']
 OUTPUT_PREFIX = f'{JOB_NAME}/{WORKFLOW_NAME}/{RUN_ID}'
 CROMWELL_OUTPUT_PREFIX = f'mutect2-chip/{OUTPUT_PREFIX}'
 CROMWELL_OUTPUT_BUCKET = _config['storage'][DATASET]['default']
-CROMWELL_OUTPUT_PATH = f'{CROMWELL_OUTPUT_BUCKET}/{CROMWELL_OUTPUT_PREFIX}'
 # OUTPUT_BUCKET = _config['storage'][DATASET]['release']  # Currently no 'release' key in config TOML
 OUTPUT_BUCKET = re.sub(r'\-main$', '-release', CROMWELL_OUTPUT_BUCKET)
 OUTPUT_PATH = f'{OUTPUT_BUCKET}/{CROMWELL_OUTPUT_PREFIX}'
@@ -24,6 +23,6 @@ b = get_batch()
 
 process_j = b.new_job('move-chip-output-files')
 
-process_j.command(f"gcloud -q auth activate-service-account --key-file=/gsa-key/key.json; gsutil mv {CROMWELL_OUTPUT_PATH} {OUTPUT_PATH}")
+process_j.command(f"gcloud -q auth activate-service-account --key-file=/gsa-key/key.json; gsutil rm -r {OUTPUT_PATH}")
 
 b.run(wait=False)
